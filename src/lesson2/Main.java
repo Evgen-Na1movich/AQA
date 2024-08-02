f1cf833b3660a62ce17d9560d969ffbb069808ce
Evgen <evgen.py86@gmail.com>
2024-08-02 16:26:19 +0300

    task_2

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,10 +1,11 @@
 package lesson2;
 
-import static lesson2.Tasks.printThreeWords;
+import static lesson2.Tasks.*;
 
 public class Main {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
 
     }
 }
--- src/lesson2/Tasks.java
+++ src/lesson2/Tasks.java
@@ -1,12 +1,23 @@
 package lesson2;
 
+import java.util.Random;
+
 public class Tasks {
-    public static void main(String[] args) {
-    }
     public static void printThreeWords() {
         System.out.println("*****task1*****");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        System.out.println("*****task2*****");
+        Random random = new Random();
+        int a = random.nextInt(201) - 100;
+        int b = random.nextInt(201) - 100;
+        int result = a + b;
+        if (result >= 0) {
+            System.out.println("Сумма положительная");
+        } else System.out.println("Сумма отрицательная");
     }
 }
