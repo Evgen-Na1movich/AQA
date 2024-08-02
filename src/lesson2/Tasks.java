b1326c5b5325fa2dbca604be88267d7c5ef272d5
Evgen <evgen.py86@gmail.com>
2024-08-02 15:52:05 +0300

    task_1

---
+++ src/lesson2/Tasks.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Tasks {
+    public static void main(String[] args) {
+    }
+    public static void printThreeWords() {
+        System.out.println("*****task1*****");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
