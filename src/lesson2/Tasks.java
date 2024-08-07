7b32ef181c0f593953953a31e331a4479417ba89
Evgen <evgen.py86@gmail.com>
2024-08-07 15:17:38 +0300

    task11

--- src/lesson2/Tasks.java
+++ src/lesson2/Tasks.java
@@ -94,4 +94,18 @@ public class Tasks {
         System.out.println("Измененный массив");
         System.out.println(Arrays.toString(array));
     }
+
+    public static void arrayChangeIfNumberLess6(){
+        System.out.println("*****task11*****");
+        int[] array = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        System.out.println("Исходный массив:");
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6){
+                array[i] = array[i] * 2;
+            }
+        }
+        System.out.println("Измененный массив");
+        System.out.println(Arrays.toString(array));
+    }
 }
